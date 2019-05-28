47b394caa4bedd7b20830c32cb8ca2621bd9a4d2
Albert Khamidullin <albert.khm@gmail.com>
2019-05-28 17:00:50 +0300

    clean up

--- s.js
+++ s.js
@@ -1,44 +1,41 @@
-
 // g grass, w water, t tree
 const world = [
-  ['g','g','g','g','g','g','g',],
-  ['g','g','g','g','g','g','g',],
-  ['g','t','t','g','w','w','g',],
-  ['g','t','g','g','w','w','g',],
-  ['g','g','g','g','w','w','g',],
-  ['g','g','g','g','g','g','g',],
+  ['g', 'g', 'g', 'g', 'g', 'g', 'g',],
+  ['g', 'g', 'g', 'g', 'g', 'g', 'g',],
+  ['g', 't', 't', 'g', 'w', 'w', 'g',],
+  ['g', 't', 'g', 'g', 'w', 'w', 'g',],
+  ['g', 'g', 'g', 'g', 'w', 'w', 'g',],
+  ['g', 'g', 'g', 'g', 'g', 'g', 'g',],
 ];
+const player = document.getElementById('player');
+const mapBox = document.getElementById('map-box');
+const cellSize = 32;
 
 function drawMap(world) {
-  const mapBox = 
-    document.getElementById('map-box');
- 
-  world.forEach((line, i) => {
-    const lineNode=document.createElement('div');
-    //console.log('line', i);
-    lineNode.className = 'line';  
+
+
+  world.forEach((line) => {
+    const lineNode = document.createElement('div');
+    lineNode.className = 'line';
     mapBox.appendChild(lineNode);
     line.forEach((obj) => {
-      const lines=document.querySelectorAll('.line');
-      const objNode= document.createElement('div')
-      objNode.className=obj
-      lines[lines.length-1].appendChild(objNode)
+      const lines = document.querySelectorAll('.line');
+      const objNode = document.createElement('div');
+      objNode.className = obj;
+      lines[lines.length - 1].appendChild(objNode)
     });
   });
 }
 
-const player= document.getElementById('player')
 
-function setPlayerPosition(p,x,y)
-{
-    const cellSize=32;
-    p.style.left=x*cellSize+'px';
-    p.style.top=y*cellSize+'px';
+function setPlayerPosition(p, x, y, cellSize) {
+  p.style.left = x * cellSize + 'px';
+  p.style.top = y * cellSize + 'px';
 }
 
 
-window.addEventListener('load', 
-function() {
-  drawMap(world)
-  setPlayerPosition(player, 2,1)
-});
+window.addEventListener('load',
+  function () {
+    drawMap(world);
+    setPlayerPosition(player, 2, 1, cellSize);
+  });
